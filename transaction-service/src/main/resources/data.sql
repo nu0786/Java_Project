199152aafdb3a8e3a58b4c2f5b703997f3e8853d
Nishant Uppal <nishantuppal@M2P-LAP-959.local>
2023-10-25 11:04:32 +0530

    Adding in memory database sript

---
+++ transaction-service/src/main/resources/data.sql
@@ -0,0 +1,31 @@
+--create account table
+CREATE TABLE accounts (
+    account_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    document_number VARCHAR(255) NOT NULL
+);
+
+-- Create operationTypes table
+CREATE TABLE operationTypes (
+    operation_type_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(255) NOT NULL
+);
+
+-- Create transactions table
+CREATE TABLE transactions (
+    transaction_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    account_id BIGINT NOT NULL,
+    operation_type_id BIGINT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    event_date TIMESTAMP NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES accounts(account_id),
+    FOREIGN KEY (operation_type_id) REFERENCES operationTypes(operation_type_id)
+);
+
+
+-- Insert data into OperationTypes
+INSERT INTO operationTypes (operation_type_id, description)
+VALUES
+(1, 'Normal Purchase'),
+(2, 'Purchase with Installments'),
+(3, 'Withdrawal'),
+(4, 'Credit Voucher');
